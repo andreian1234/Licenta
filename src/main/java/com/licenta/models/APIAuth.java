ef566936655cccafd30d058f4ffe85a30b8c310b
andreian1234 <andreiancuta6@gmail.com>
2021-08-15 12:41:31 +0300

    Dependente

---
+++ src/main/java/com/licenta/models/APIAuth.java
@@ -0,0 +1,14 @@
+package com.licenta.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class APIAuth {
+
+private final String appKey;
+private final String appId;
+}
